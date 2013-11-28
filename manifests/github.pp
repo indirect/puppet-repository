daa462f848dafbdf080368a8a315fc914d52cf8d
Andre Arko <andre@arko.net>
2013-11-28 00:06:54 -0800

    implement repository::github using hash_compact

--- manifests/github.pp
+++ manifests/github.pp
@@ -1,14 +1,21 @@
 # check out name/repo into ~/src/name/repo
 define repository::github(
-  $path = $title, $source = undef, $protocol = undef,
-  $user = undef, $config = undef, $extra = undef, $force = undef
+  $path     = "${boxen::config::srcdir}/$title",
+  $source   = $title,
+  $protocol = undef,
+  $user     = undef,
+  $config   = undef,
+  $extra    = undef,
+  $force    = undef
 ) {
-  repository { "${boxen::config::srcdir}/$path":
-    source => $path,
+  $options = hash_compact({
+    path => $path,
+    source => $source,
     protocol => $protocol,
     user => $user,
     config => $config,
     extra => $extra,
     force => $force
-  }
+  })
+  ensure_resource("repository", "$path", $options)
 }
